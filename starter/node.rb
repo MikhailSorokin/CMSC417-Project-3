179a360bab5e28055bb2b6c087cd02236c2dcabc
MikhailSorokin <sorokinmike@yahoo.com>
2017-11-14 13:48:25 -0500

    Pushing some changes that are not yet finished to look at.

--- starter/node.rb
+++ starter/node.rb
@@ -1,7 +1,9 @@
+require 'socket' #Required import to allow server connection 
+
 $port = nil
 $hostname = nil
-
-
+$server = nil
+$BUF_SIZE = 1024
 
 # --------------------- Part 1 --------------------- # 
 class RoutingInfo
@@ -18,7 +20,40 @@ class RoutingInfo
 end
 
 def edgeb(cmd)
-	STDOUT.puts "EDGE: not implemented"
+	#Parse
+	srcIP = cmd[0]
+	destIP = cmd[1]
+	destNode = cmd[2]
+
+	clientPort = nil
+
+	serverRI = RoutingInfo.new($hostname, destNode, destNode, 1)
+
+	#Establish a connection between two nodes.
+	$server = TCPServer.new(srcIP, $port)
+	$socketToNode[$server] = serverRI.src
+
+	loop do
+		#There will be three threads
+		#(1) - Listening Thread - (Accepts and receives new connections)
+		#(2) - Receiving Thread - (Polls existing connections for new info)
+		#(3) - MSG Handler - (Parses and handles input and pushes to output buffers)
+
+		#New thread will allow us to establish multiple connections from clients
+	  	Thread.start($server.accept) do |client|
+	  		$socketToNode[client] = destNode
+	  		clientPort = client.addr[1]
+	    	client.write(serverRI.to_s) #send information to the client
+	    end
+
+	    #Another thread for receiving
+	    Thread.start(clientSocket = TCPSocket.new(destIP, clientPort))
+			recvStr = clientSocket.recv(BUF_SIZE)	
+
+		strArray = recvStr.split(",")
+		clientRI = RoutingInfo.new(strArray[1], strArray[0],
+		strArray[2],strArray[3]) #server src and dst are in reverse
+  	end
 end
 
 def dumptable(cmd)
@@ -26,7 +61,11 @@ def dumptable(cmd)
 end
 
 def shutdown(cmd)
-	STDOUT.puts "SHUTDOWN: not implemented"
+	#Create a connection for each TCP Socket again
+
+	$socketToNode.each do |client|
+		client.close
+	end
 	exit(0)
 end
 
@@ -100,13 +139,13 @@ def main()
 end
 
 def setup(hostname, port, nodes, config)
-	$hostname = hostname
+	$hostname = hostname #this is the SRC node
 	$port = port
 
 	#set up ports, server, buffers
-	
+	$BUF_SIZE = 1023
+
 	$socketToNode = {} #Hashmap to index node by socket
-	
 
 	main()
 
